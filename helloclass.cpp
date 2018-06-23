f32b301a48b8cce41fb6bb91b3641d5b57c5083c
SnowGril <745493262@qq.com>
2018-06-23 11:04:22 +0800

    happy!

---
+++ helloclass.cpp
@@ -0,0 +1,18 @@
+#include "helloClass.h"
+
+HelloClass::HelloClass()
+{
+         cout<<"I am being created!"<<endl;
+}
+
+HelloClass::~HelloClass()
+{
+          cout<<"I am being destroyed!"<<endl;
+}
+
+void HelloClass::sayHello()
+{
+   cout<<"Hello Class!"<<endl;
+}
+
+
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include "helloClass.h"
+
+using namespace std;
+
+int main()
+{
+   HelloClass obj;
+
+  obj. sayHello();
+   return 0;
+}
+
+
